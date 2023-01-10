7a400bd3f580054216eff478e92c04e33a967862
liuzhen3 <liuzhen3@360.cn>
2023-01-10 10:45:39 +0000

    feature: mv fibers from main.cpp

---
+++ fibers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <folly/fibers/Baton.h>
+#include <folly/fibers/Fiber.h>
+#include <folly/fibers/FiberManager.h>
+#include <folly/fibers/FiberManagerMap.h>
+#include <folly/fibers/Promise.h>
+#include <folly/io/async/EventBase.h>
+#include "fibers.h"
+
+void fibers() {
+  std::cout << "Hello, World!" << std::endl;
+  folly::EventBase evb;
+  auto& fiberManager = folly::fibers::getFiberManager(evb);
+  folly::fibers::Baton baton;
+  fiberManager.addTask([&]() {
+    std::cout << "Task 1: start" << std::endl;
+    baton.wait();
+    std::cout << "Task 1: after baton.wait()" << std::endl;
+  });
+
+  fiberManager.addTask([&]() {
+    std::cout << "Task 2: start" << std::endl;
+    baton.post();
+    std::cout << "Task 2: after baton.post()" << std::endl;
+  });
+
+  evb.loop();
+}
--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
-#include <folly/io/async/EventBase.h>
-#include <folly/fibers/Baton.h>
-#include <folly/fibers/FiberManager.h>
-#include <folly/fibers/FiberManagerMap.h>
-#include <folly/fibers/Fiber.h>
+#include <folly/experimental/io/IoUringBackend.h>
+#include "fibers.h"
 
+int main() { std::cout << "Hello, World!" << std::endl;
+  fibers();
 
-
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  folly::EventBase evb;
-  auto& fiberManager = folly::fibers::getFiberManager(evb);
-  folly::fibers::Baton baton;
-  fiberManager.addTask([&]() {
-    std::cout << "Task 1: start" << std::endl;
-    baton.wait();
-    std::cout << "Task 1: after baton.wait()" << std::endl;
-  });
-
-  fiberManager.addTask([&]() {
-    std::cout << "Task 2: start" << std::endl;
-    baton.post();
-    std::cout << "Task 2: after baton.post()" << std::endl;
-  });
-
-  evb.loop();
-}
+ }
