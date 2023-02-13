125a20b6535a866b554421d08fa5a3e9098ef8ab
liuzhen3 <liuzhen3@360.cn>
2023-02-13 12:41:38 +0000

    feature: cmake liburing using external project

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-#include <folly/executors/ThreadedExecutor.h>
-#include <folly/futures/Future.h>
-#include <folly/init/Init.h>
-
 #include <iostream>
 
-#include "fibers.h"
+// #include "fibers.h"
 using namespace std;
 
 int main(int argc, char** argv) {
-  folly::Init Init(&argc, &argv);
-  RunFiber();
+  // folly::Init Init(&argc, &argv);
+  // RunFiber();
+  std::cout << "Hello World!" << std::endl;
 }
