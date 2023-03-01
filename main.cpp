7135954680e6bf3bf9f3d66fb2708ea9f9b82065
liuzhen3 <liuzhen3@360.cn>
2023-03-01 13:32:02 +0000

    feature: using external project compile folly

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <glog/logging.h>
 
 // #include "fibers.h"
 using namespace std;
@@ -7,4 +8,5 @@ int main(int argc, char** argv) {
   // folly::Init Init(&argc, &argv);
   // RunFiber();
   std::cout << "Hello World!" << std::endl;
+  LOG(INFO) << "Hello World!";
 }
