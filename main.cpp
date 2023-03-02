f408a3b391f38075d80eed7d0b6ab4756cd6d81d
liuzhen3 <liuzhen3@360.cn>
2023-03-02 03:07:16 +0000

    feature: fiber run successful!

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <glog/logging.h>
+#include <folly/init/Init.h>
 
-// #include "fibers.h"
+#include "fibers.h"
 using namespace std;
 
 int main(int argc, char** argv) {
-  // folly::Init Init(&argc, &argv);
-  // RunFiber();
-  std::cout << "Hello World!" << std::endl;
+  folly::Init Init(&argc, &argv);
   LOG(INFO) << "Hello World!";
+  RunFiber();
 }
